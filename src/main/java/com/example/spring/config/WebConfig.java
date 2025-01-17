82524a8cc0998687aad4d575abade7fefa6c7ac1
ifi999 <kkwkjm4231@gmail.com>
2025-01-18 08:44:43 +0900

    application/json 처리를 위한 WebConfig 작성

---
+++ src/main/java/com/example/spring/config/WebConfig.java
@@ -0,0 +1,29 @@
+package com.example.spring.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@EnableWebMvc
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public MappingJackson2HttpMessageConverter jsonConverter() {
+        final ObjectMapper objectMapper = new ObjectMapper();
+
+        return new MappingJackson2HttpMessageConverter(objectMapper);
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(jsonConverter());
+    }
+
+}
