7cf4819e5a3128ab09c5ecc79749f442652d798f
ifi999 <kkwkjm4231@gmail.com>
2025-01-18 08:43:21 +0900

    OrderService 작성

---
+++ src/main/java/com/example/spring/service/OrderService.java
@@ -0,0 +1,52 @@
+package com.example.spring.service;
+
+import com.example.spring.controller.dto.CreateOrder;
+import com.example.spring.repository.OrderJpaRepository;
+import com.example.spring.repository.entity.Order;
+import com.example.spring.repository.entity.OrderItem;
+import com.example.spring.repository.entity.OrderStatus;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Transactional
+@Service
+public class OrderService {
+
+    private final OrderJpaRepository orderJpaRepository;
+
+    public OrderService(final OrderJpaRepository orderJpaRepository) {
+        this.orderJpaRepository = orderJpaRepository;
+    }
+
+    public Order getOrder(Long id) {
+        return orderJpaRepository.findById(id);
+    }
+
+    public void createOrder(final CreateOrder.HttpRequest request) {
+        final List<OrderItem> orderItems = request.getItems().stream()
+            .map(item -> new OrderItem(
+                item.getProductId(),
+                item.getProductName(),
+                item.getOption(),
+                item.getPrice(),
+                item.getQuantity()
+            ))
+            .toList();
+
+        final Order order = new Order(
+            request.getCustomerId(),
+            request.getTotalPrice(),
+            OrderStatus.PENDING,
+            orderItems
+        );
+
+        orderJpaRepository.save(order);
+    }
+
+    public List<Order> listOrders() {
+        return orderJpaRepository.findAll();
+    }
+
+}
