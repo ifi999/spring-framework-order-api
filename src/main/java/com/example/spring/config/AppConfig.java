e80801a1d659831fd8c47b642ee2e4722cbf737d
ifi999 <kkwkjm4231@gmail.com>
2025-01-18 10:31:01 +0900

    Swagger2 설정 작성

--- src/main/java/com/example/spring/config/AppConfig.java
+++ src/main/java/com/example/spring/config/AppConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
-@Import({JpaConfig.class, DataSourceConfig.class})
+@Import({JpaConfig.class, DataSourceConfig.class, SwaggerConfig.class})
 @ComponentScan(basePackages = "com.example.spring")
 @Configuration
 public class AppConfig {
--- src/main/java/com/example/spring/config/WebConfig.java
+++ src/main/java/com/example/spring/config/WebConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -24,6 +25,15 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
         converters.add(jsonConverter());
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+            .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+            .addResourceLocations("classpath:/META-INF/resources/webjars/");
+
     }
 
 }
