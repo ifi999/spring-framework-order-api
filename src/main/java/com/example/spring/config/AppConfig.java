2d07c2c9319cba06a6e778c68409e51e30d02fb7
ifi999 <kkwkjm4231@gmail.com>
2025-01-18 09:41:49 +0900

    AppConfig에 JPA 및 DataSource 설정 추가

--- src/main/java/com/example/spring/config/AppConfig.java
+++ src/main/java/com/example/spring/config/AppConfig.java
@@ -2,7 +2,9 @@ package com.example.spring.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
+@Import({JpaConfig.class, DataSourceConfig.class})
 @ComponentScan(basePackages = "com.example.spring")
 @Configuration
 public class AppConfig {
